eeb0d470b03d54609c44c8d7a632f597c3924bdd
naiv <nazarivankiv1@gmail.com>
2023-12-14 17:23:44 +0200

    added .flake8 and updated files to keep flake requirements

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "customer",
     "book_service",
     "borrowing_service",
     "rest_framework"
@@ -101,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "customer.Customer"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
