99c3abfe9d0c4e96f39d9ce6fd66c5c676221dcb
naiv <nazarivankiv1@gmail.com>
2023-12-14 14:49:15 +0200

    setup ViewSet for Books and endpoints for it

--- book_service/views.py
+++ book_service/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from book_service.models import Book
+from book_service.permissions import IsAdminOrReadOnly
+from book_service.serializers import BookListSerializer, BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return BookSerializer
