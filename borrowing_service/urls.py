9fe7279bcc54279bfa9fde0b043c5637bee1d605
Rost <rv4@ukr.net>
2023-12-15 10:56:27 +0200

    import implemented borrowing_return function

--- borrowing_service/urls.py
+++ borrowing_service/urls.py
@@ -2,12 +2,14 @@ from django.urls import path, include
 from rest_framework import routers
 
 from borrowing_service import views
+from borrowing_service.views import borrowing_return
 
 router = routers.DefaultRouter()
 router.register("borrowings", views.BorrowingViewSet)
 
 urlpatterns = [
-    path("/", include(router.urls))
+    path("/", include(router.urls)),
+    path("/borrowings/<int:pk>/return/", borrowing_return, name="borrowings-return")
 ]
 
 app_name = "borrowing_service"
