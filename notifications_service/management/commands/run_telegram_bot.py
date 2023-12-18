a1d54c4e5b6b2767356afd5ebeb5f4082c64bc4c
Katherine-Greg <katiasimpson17@gmail.com>
2023-12-18 08:32:04 +0200

    Update telegram bot commands

--- notifications_service/management/commands/run_telegram_bot.py
+++ notifications_service/management/commands/run_telegram_bot.py
@@ -2,8 +2,10 @@ import telebot
 from django.core.management import BaseCommand
 
 from core.settings import CHAT_ID, TELEGRAM_BOT_TOKEN
-from notifications_service.commands import welcome_message, help_information
-# from django_q.tasks import async_task
+from notifications_service.bot_commands import (welcome_message,
+                                                help_information,
+                                                user_borrowings,
+                                                is_user)
 
 
 class Command(BaseCommand):
@@ -14,16 +16,24 @@ class Command(BaseCommand):
             bot.polling()
 
         @bot.message_handler(commands=["start"])
-        def send_welcome(message) -> None:
+        def login(message):
+            if not is_user(message):
+                bot.send_message(message.chat.id,
+                                 text="Enter your email to login: ")
+                bot.register_next_step_handler(message, login)
+            else:
+                authorised(message)
+
+        def authorised(message):
             welcome_message(bot, message)
 
         @bot.message_handler(commands=["help"])
         def send_help_information(message: telebot.types.Message) -> None:
             help_information(bot, message)
 
-        # @bot.message_handler(commands=["my_borrowings"])
-        # def send_users_borrowings(message: telebot.types.Message) -> None:
-        #     async_task(user_borrowings, bot, message)
+        @bot.message_handler(commands=["my_borrowings"])
+        def send_users_borrowings(message: telebot.types.Message) -> None:
+            user_borrowings(bot, message)
 
         def send_telegram_notification(message):
             bot.send_message(CHAT_ID, text=message)
