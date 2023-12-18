19978dcc45e78987d20180902db8ba62ebe7fb76
Katherine-Greg <katiasimpson17@gmail.com>
2023-12-18 02:55:39 +0200

    Created telegram user model

--- notifications_service/models.py
+++ notifications_service/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from customer.models import Customer
+
+
+class TelegramUser(models.Model):
+    user_id = models.ForeignKey(Customer,
+                                on_delete=models.CASCADE,
+                                related_name="customer")
+    chat_id = models.IntegerField()
+    chat_url = models.URLField()
